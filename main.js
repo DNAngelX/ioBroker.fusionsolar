ad687eb6bcea58435f671ae8d4beca5980ba7430
stolly82 <stolly82@web.de>
2023-04-16 14:54:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -201,6 +201,9 @@ class FusionSolarConnector extends utils.Adapter {
                                 //UNKNOWN
                                 if(skipUnknownDevices) continue;
                             }
+                            var test = getState('fusionsolar.0.NE=35841112.1000000035841116.frequency');
+                            this.log.debug('TEST - ' + JSON.stringify(test));
+                            this.log.debug('TEST1 - ' + JSON.stringify(channelParentPath + channelName));
                             
                             // Here should be the value from deviceInfo.frequency in frequency
                             
