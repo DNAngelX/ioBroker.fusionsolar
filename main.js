6b875336d9637aa98eeafa54a29e71098423c42d
stolly82 <stolly82@web.de>
2023-04-19 20:40:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -211,8 +211,8 @@ class FusionSolarConnector extends utils.Adapter {
                             
                             let deviceId = deviceInfo.id;
 
-                            const objModel = await this.getStateAsync(myStation + '.' + 'plantCode');
-                            this.log.debug('GETMODELFROMAPI: objModel: ' + JSON.stringify(objModel) + 'Val: ' + objModel + 'PH: ' + myStation);
+                            const objModel = await this.getStateAsync(myStation + '.' + deviceId + 'frequency');
+                            this.log.debug('GETMODELFROMAPI: objModel: ' + JSON.stringify(objModel) + 'Val: ' + objModel.val + 'PH: ' + myStation);
 
                             
                             if (counter == 0)
